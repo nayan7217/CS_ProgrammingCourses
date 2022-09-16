8d2fd4f3f2bc2f0f2e636e2f4d75f29132ae224b
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-16 12:31:11 +0530

    New Rository Created

---
+++ C# Beginners/CS_ExerciseModuleNo_6Q5/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace question5
+{
+    class Solution
+    {
+        public static void Main()
+        {
+            bool isNotCapableString = true;
+
+            string inputString = "";
+            while (isNotCapableString)
+            {
+                Console.WriteLine("Enter the element of the list");
+                string input = Console.ReadLine();
+                if (iscount5(input))
+                {
+                    isNotCapableString = false;
+                    inputString += input;
+                }
+                else
+                {
+                    Console.WriteLine("Length of the string is not 5");
+        
+                }
+
+            } 
+            
+            var stringArray = inputString.Split(' ');
+            List<int > numbers = new List<int>();
+            for(int i = 0; i < stringArray.Length; i++)
+            {
+                numbers.Add(Convert.ToInt32(stringArray[i]));
+            }
+
+
+            numbers.Sort();
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+        }
+        public static bool iscount5(String input)
+        {
+            int Count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    Count++;
+                }
+            }
+            return Count == 5 ? true : false;
+        }
+    }
+}
---
+++ C# Beginners/CS_ExerciseModuleNo_8Q3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Console.WriteLine("Enter the hour and minuts");
+
+string input = Console.ReadLine();
+string[] inputArray = input.Split(':');
+
+
+
+var Time = TimeSpan.Parse(Console.ReadLine());
+
+Console.WriteLine(Time.Hours);
+if (Convert.ToInt32(inputArray[0]) <00 || Convert.ToInt32(inputArray[0]) > 24)
+{
+    Console.WriteLine("Invalid Time");
+}
+else
+{
+    Console.WriteLine("OK");
+}
