33a20d3515960ecb559acfac8a80ed8383495934
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-16 16:24:51 +0530

    Complete

---
+++ CS_IntermediateCourse/CS_Module2_Exercise_Q2/Post.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Module2_Exercise_Q2
+{
+    public class Post
+    {
+        public string title { get; set; }
+
+        public string  description { get; set; }
+
+        public DateTime createdDate{ get; set; }
+
+        public int vote { get; set; }
+
+
+        public Post(string postTitle, string postDescription, DateTime createdON)
+        {
+            this.title = postTitle;
+            this.description = postDescription;
+            this.createdDate = createdON;
+        }
+
+        public void UpVote()
+        {
+            this.vote ++;
+            return;
+        }
+        public void DawnVote()
+        {
+            this.vote--;
+            return ;
+        }
+        public void PrintVote()
+        {
+            Console.WriteLine($"Total No of vote for the Post {this.title} are {this.vote}");
+        }
+    }
+}
---
+++ CS_IntermediateCourse/CS_Module2_Exercise_Q2/Program.cs
@@ -0,0 +1,32 @@
+﻿using CS_Module2_Exercise_Q2;
+
+Post post1 = new Post("I love my India", "Gratitude towards Country", DateTime.Now);
+int ch;
+do
+{
+    ch = 1;
+    Console.WriteLine("1. Up vote");
+    Console.WriteLine("2. Dawn vote");
+    Console.WriteLine("3. Print the total Votes");
+    int op = Convert.ToInt32(Console.ReadLine());
+
+    switch (op)
+    {
+        case 1: post1.UpVote();
+            Console.WriteLine("Successfully Voted to the Post");
+            break;
+        case 2:post1.DawnVote();
+            Console.WriteLine("Successfully Voted to the Post");
+            break;
+        case 3:post1.PrintVote();
+            break;
+    }
+    Console.WriteLine("Enter 1 to continue or anything to exit");
+    ch = Convert.ToInt32(Console.ReadLine());
+
+
+    
+}while (ch == 1);
+
+
+
