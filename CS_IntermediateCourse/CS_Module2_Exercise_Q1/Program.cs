66f182835c38b2c028a1b1bd4f826067a8200f26
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-16 16:47:10 +0530

    Class

---
+++ CS_IntermediateCourse/CS_Module2_Exercise_Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using CS_Module2_Exercise_Q1;
+
+Stopwatch stopwatch = new Stopwatch();
+bool isStarted = false;
+
+while(true)
+{
+    Console.WriteLine("1. Start ");
+    Console.WriteLine("2. End ");
+    int op = Convert.ToInt32(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            if (isStarted == false)
+            {
+                stopwatch.Start();
+                isStarted = true;
+            }
+            else
+            {
+                Console.WriteLine("StopWatch Already Started");
+            }
+            break;
+        case 2:
+            stopwatch.End();
+            return;
+
+
+    }
+}
---
+++ CS_IntermediateCourse/CS_Module2_Exercise_Q1/Stopwatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Module2_Exercise_Q1
+{
+    public class Stopwatch
+    {
+        DateTime timeStart;
+
+        DateTime timeEnd;
+
+        public void Start()
+        {
+            this.timeStart = DateTime.Now;
+            
+        }
+        public void End()
+        {
+            this.timeEnd = DateTime.Now;
+            string str = $"Time Span : {timeEnd.Hour - timeStart.Hour}:{timeEnd.Second - timeEnd.Second}";
+            Console.WriteLine(str);
+            return;
+        }
+    
+    }
+}
