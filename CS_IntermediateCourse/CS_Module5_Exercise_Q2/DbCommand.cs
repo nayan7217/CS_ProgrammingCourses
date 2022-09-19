35078edf8a05a23d056936bbbb2e2968d9b00903
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-19 16:25:33 +0530

    Done

---
+++ CS_IntermediateCourse/CS_Module5_Exercise_Q2/DbCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS_Module5_Exercise_Q2;
+
+namespace CS_Module5_Exercise_Q2
+{
+    public class DbCommand
+    {
+        public bool isConnected;
+
+
+        public DbCommand(SqlConnection obj)
+        {
+            isConnected = obj.isConnected;
+        }
+
+        public void Execute(SqlCommand cmd)
+        {
+
+        }
+    }
+}
---
+++ CS_IntermediateCourse/CS_Module5_Exercise_Q2/Program.cs
@@ -0,0 +1,8 @@
+﻿using CS_Module5_Exercise_Q2;
+
+TimeSpan time = new TimeSpan(0, 2, 30);
+
+string Connection = Console.ReadLine();
+SqlConnection sqlConnection = new SqlConnection(Connection, time);
+
+OracleConnection oracleConnection = new OracleConnection(Connection, time);
